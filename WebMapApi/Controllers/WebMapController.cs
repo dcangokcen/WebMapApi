972b50a6e84b334691f7355db8e7e2c57641743b
Dogus Can GOKCEN <dogus_can91@hotmail.com>
2024-03-31 21:07:59 +0300

    Add project files.

---
+++ WebMapApi/Controllers/WebMapController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using WebMapApi.Models;
+using Newtonsoft;
+
+namespace WebMapApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+
+	public class WebMapController : ControllerBase
+	{
+		private readonly string pointsFilePath = "points.json";
+
+		[HttpPost("/save-point")]
+		public IActionResult SavePoint(PointModel point)
+		{
+			try
+			{
+				var points = new List<PointModel>();
+				if (System.IO.File.Exists(pointsFilePath))
+				{
+					points = Newtonsoft.Json.JsonConvert.DeserializeObject<List<PointModel>>(System.IO.File.ReadAllText(pointsFilePath));
+				}
+
+				point.Id = points.Count > 0 ? points.Max(p => p.Id) + 1 : 0; 
+				points.Add(point);
+
+				System.IO.File.WriteAllText(pointsFilePath, Newtonsoft.Json.JsonConvert.SerializeObject(points, Newtonsoft.Json.Formatting.Indented));
+
+				return Ok(new { message = "Point saved successfully" });
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, new { error = "Internal Server Error" });
+			}
+		}
+
+		[HttpGet("/points")]
+		public IActionResult GetPoints()
+		{
+			try
+			{
+				var points = new List<PointModel>();
+				if (System.IO.File.Exists(pointsFilePath))
+				{
+					points = Newtonsoft.Json.JsonConvert.DeserializeObject<List<PointModel>>(System.IO.File.ReadAllText(pointsFilePath));
+				}
+				return Ok(points);
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, new { error = "Internal Server Error" });
+			}
+		}
+
+		[HttpDelete("/delete-point/{id}")]
+		public IActionResult DeletePoint(int id)
+		{
+			try
+			{
+				var points = new List<PointModel>();
+				if (System.IO.File.Exists(pointsFilePath))
+				{
+					points = Newtonsoft.Json.JsonConvert.DeserializeObject<List<PointModel>>(System.IO.File.ReadAllText(pointsFilePath));
+				}
+
+				var pointToRemove = points.FirstOrDefault(p => p.Id == id);
+				if (pointToRemove != null)
+				{
+					points.Remove(pointToRemove);
+					System.IO.File.WriteAllText(pointsFilePath, Newtonsoft.Json.JsonConvert.SerializeObject(points, Newtonsoft.Json.Formatting.Indented));
+					return Ok(new { message = "Point deleted successfully" });
+				}
+				else
+				{
+					return NotFound(new { error = "Point not found" });
+				}
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, new { error = "Internal Server Error" });
+			}
+		}
+	}
+}
---
+++ WebMapApi/Models/PointModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebMapApi.Models
+{
+	public class PointModel
+	{
+		public int Id { get; set; }
+		public string Lat { get; set; }
+		public string Lng { get; set; }
+		public DateTime DateTime { get; set; }
+	}
+}
