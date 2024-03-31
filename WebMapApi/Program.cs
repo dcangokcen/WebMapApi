bb937b53792e633639891c6f76330ed0b3ac9478
Dogus Can GOKCEN <d.cangokcen@gmail.com>
2024-03-31 21:23:45 +0300

    Initial commit

--- WebMapApi/Program.cs
+++ WebMapApi/Program.cs
@@ -1,13 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Enable CORS
 builder.Services.AddCors(options =>
 {
 	options.AddPolicy("CorsPolicy",
@@ -21,7 +19,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
@@ -32,7 +29,6 @@ app.UseHttpsRedirection();
 
 app.UseRouting();
 
-// Use CORS middleware
 app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
